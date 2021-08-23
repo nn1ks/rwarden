bcfe0484bf747eebc225281a5576d7f334d504a6
Niklas Sauter <niklas@n1ks.net>
2021-08-23 04:33:23 +0200

    Fix deserialization of `response::ErrorTwoFactorProvider`

--- rwarden/src/response.rs
+++ rwarden/src/response.rs
@@ -1,7 +1,7 @@
 //! Module for responses returned from the Bitwarden API.
 
-use serde::Deserialize;
-use std::collections::HashMap;
+use serde::{de, Deserialize, Deserializer};
+use std::{collections::HashMap, fmt};
 use thiserror::Error;
 
 #[derive(Deserialize)]
@@ -17,14 +17,14 @@ struct InnerError {
     validation_errors: Option<HashMap<String, Vec<String>>>,
     error_model: Option<InnerErrorModel>,
     #[serde(rename = "TwoFactorProviders2")]
-    two_factor_providers: Option<HashMap<i32, ErrorTwoFactorProvider>>,
+    two_factor_providers: Option<ErrorTwoFactorProviderMap>,
 }
 
 impl From<InnerError> for Error {
     fn from(value: InnerError) -> Self {
         if let Some(two_factor_providers) = value.two_factor_providers {
             return Self::TwoFactorRequired {
-                two_factor_providers,
+                two_factor_providers: two_factor_providers.0,
             };
         }
         Self::Other {
@@ -44,7 +44,7 @@ pub enum Error {
     /// Two factor authentication is required.
     #[error("Two factor authentication is required")]
     TwoFactorRequired {
-        two_factor_providers: HashMap<i32, ErrorTwoFactorProvider>,
+        two_factor_providers: Vec<ErrorTwoFactorProvider>,
     },
     /// An unknown error occurred.
     #[error("Unknown error: {}", .message)]
@@ -55,19 +55,129 @@ pub enum Error {
 }
 
 /// Provider for two factor authentication.
-#[derive(Debug, Clone, PartialEq, Eq, Hash, Deserialize)]
-#[serde(untagged)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum ErrorTwoFactorProvider {
-    #[serde(rename_all = "PascalCase")]
-    Email { email: String },
-    #[serde(rename_all = "PascalCase")]
+    Authenticator,
+    Email {
+        email: String,
+    },
+    Duo {
+        host: String,
+        signature: String,
+    },
+    YubiKey {
+        nfc: bool,
+    },
     U2f {
         challenges: Vec<ErrorTwoFactorProviderU2fChallenge>,
     },
-    #[serde(rename_all = "PascalCase")]
-    Duo { host: String, signature: String },
-    #[serde(rename_all = "PascalCase")]
-    Yubikey { nfc: bool },
+    WebAuthn,
+}
+
+impl From<&ErrorTwoFactorProvider> for crate::TwoFactorProvider {
+    fn from(value: &ErrorTwoFactorProvider) -> Self {
+        match value {
+            ErrorTwoFactorProvider::Authenticator => Self::Authenticator,
+            ErrorTwoFactorProvider::Email { .. } => Self::Email,
+            ErrorTwoFactorProvider::Duo { .. } => Self::Duo,
+            ErrorTwoFactorProvider::YubiKey { .. } => Self::YubiKey,
+            ErrorTwoFactorProvider::U2f { .. } => Self::U2f,
+            ErrorTwoFactorProvider::WebAuthn => Self::WebAuthn,
+        }
+    }
+}
+
+impl From<ErrorTwoFactorProvider> for crate::TwoFactorProvider {
+    fn from(value: ErrorTwoFactorProvider) -> Self {
+        Self::from(&value)
+    }
+}
+
+struct ErrorTwoFactorProviderMap(Vec<ErrorTwoFactorProvider>);
+
+impl<'de> Deserialize<'de> for ErrorTwoFactorProviderMap {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct Visitor;
+
+        impl<'de> de::Visitor<'de> for Visitor {
+            type Value = ErrorTwoFactorProviderMap;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("two factor provider map")
+            }
+
+            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+            where
+                A: de::MapAccess<'de>,
+            {
+                let mut providers = Vec::with_capacity(map.size_hint().unwrap_or(0));
+                while let Some(key) = map.next_key::<&str>()? {
+                    let provider = match key {
+                        "0" => {
+                            let _value = map.next_value::<serde_json::Value>()?;
+                            ErrorTwoFactorProvider::Authenticator
+                        }
+                        "1" => {
+                            #[derive(Deserialize)]
+                            #[serde(rename_all = "PascalCase")]
+                            struct Response {
+                                email: String,
+                            }
+                            let value = map.next_value::<Response>()?;
+                            ErrorTwoFactorProvider::Email { email: value.email }
+                        }
+                        "2" => {
+                            #[derive(Deserialize)]
+                            #[serde(rename_all = "PascalCase")]
+                            struct Response {
+                                host: String,
+                                signature: String,
+                            }
+                            let value = map.next_value::<Response>()?;
+                            ErrorTwoFactorProvider::Duo {
+                                host: value.host,
+                                signature: value.signature,
+                            }
+                        }
+                        "3" => {
+                            #[derive(Deserialize)]
+                            #[serde(rename_all = "PascalCase")]
+                            struct Response {
+                                nfc: bool,
+                            }
+                            let value = map.next_value::<Response>()?;
+                            ErrorTwoFactorProvider::YubiKey { nfc: value.nfc }
+                        }
+                        "4" => {
+                            #[derive(Deserialize)]
+                            #[serde(rename_all = "PascalCase")]
+                            struct Response {
+                                challenges: Vec<ErrorTwoFactorProviderU2fChallenge>,
+                            }
+                            let value = map.next_value::<Response>()?;
+                            ErrorTwoFactorProvider::U2f {
+                                challenges: value.challenges,
+                            }
+                        }
+                        "7" => ErrorTwoFactorProvider::WebAuthn,
+                        _ => {
+                            return Err(de::Error::invalid_value(
+                                de::Unexpected::Str(key),
+                                &"a valid two factor provider id",
+                            ))
+                        }
+                    };
+                    providers.push(provider);
+                }
+                Ok(ErrorTwoFactorProviderMap(providers))
+            }
+        }
+
+        deserializer.deserialize_map(Visitor)
+    }
 }
 
 /// Challenge of U2f two factor authentication.
